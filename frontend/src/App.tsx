7cf4f69226765dd1b67069760cf559a7a7c2988b
ChanceBarimbao <chancevillasbarimbao@gmail.com>
2025-07-25 11:27:58 -0700

    added group perms and missing mnc modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { checkForMetaNetClient, NoMncModal } from 'metanet-react-prompt'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Layout from './Layout'
 import Account from './pages/Account'
 import Details from './pages/Details'
 import Store from './pages/Store'
 import UploadFile from './pages/UploadFile'
-
+import {WalletClient} from '@bsv/sdk'
+const [MNCmissing, setMNCMissing] = useState<boolean>(false)
 const App: React.FC = () => {
+   useEffect(() => {
+      const intervalId = setInterval(async () => {
+      const hasMNC = await checkForMetaNetClient()
+      if(hasMNC===0){
+        setMNCMissing(true)
+      }else{
+        clearInterval(intervalId)
+        setMNCMissing(false)
+        const Client = new WalletClient
+        Client.waitForAuthentication()
+      }
+    },1000)
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+  
   return (
     <Router>
       <Routes>
@@ -17,6 +36,7 @@ const App: React.FC = () => {
           <Route path="account" element={<Account />} />
         </Route>
       </Routes>
+      <NoMncModal appName={'Pollr'} open={MNCmissing} onClose={() => setMNCMissing(false)} />
     </Router>
   )
 }
